7389f96954f76b934314020cc3fc1f1dc0e9cf6e
Michael Flis <michaelflis07@gmail.com>
2018-01-07 10:56:18 -0800

    Add explodingDancer subclass, fixed comments

--- src/explodingDancer.js
+++ src/explodingDancer.js
@@ -14,10 +14,7 @@ ExplodingDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this, this.timeBetweenSteps);
   var context = this;
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  // context.$node.fadeOut( function() { context.$node.fadeIn(); }.bind(context));
+
   context.$node.fadeToggle("slow", "linear");
   context.$node.effect("explode", "slow");
 
